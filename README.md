f2fd772767cf39c023f9e23b08426e03a8541c85
ditrix <ditrix2006@gmail.com>
2023-12-12 20:23:59 +0200

    init dirty

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[Create Laravel 10 Vue JS CRUD Single Page Application] (https://www.positronx.io/create-laravel-vue-js-crud-single-page-application/)
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all()->toArray();
+
+        return array_reverse($products);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $product = new Product([
+            'name' => $request->input('name'),
+            'detail'=> $request->input('detail'),
+        ]);
+        $product->save();
+
+        return response()->json('Product created');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+
+        return response()->json($product);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all);
+
+        return response()->json('Product updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+
+        return response()->json('Product deleted');
+    }
+}
